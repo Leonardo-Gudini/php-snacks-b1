8fba22643f5c9e027d7e20a960bf36a157a50b64
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-12 17:13:52 +0200

    add: snack 4

--- snack4.php
+++ snack4.php
@@ -6,9 +6,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Array numeri casuali</title>
 </head>
 <body>
+
+    <?php
+    $casualNumbers = [];
+
+    for($i = 0; count($casualNumbers) < 15 ; $i++){
+
+        $number = rand( 1, 100);
+
+        if(array_search($number, $casualNumbers) == false){
+            $casualNumbers[] = $number;
+        }
+    };
+    
+
+    var_dump($casualNumbers);
+    ?>
     
 </body>
 </html>
